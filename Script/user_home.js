e27cc22dc360c3c9db75372cc251c1d6c10da65b
MoadFarhat <103571780+MoadFarhat@users.noreply.github.com>
2022-06-02 11:38:58 +0300

    تعديل الصفحة
    تعديل الإحصائيات

--- Script/user_home.js
+++ Script/user_home.js
@@ -12,8 +12,35 @@ closeBtn.addEventListener('click',()=>{
 })
 // chang mode
 themeToggler.addEventListener('click',()=>{
-  
     document.body.classList.toggle('dark-theme-variables');
-    
-   
 })
+let number=document.getElementById('numberprogresincom');
+let counter=0;
+setInterval(()=>{
+    if(counter==65){
+    clearInterval();
+    }
+    else{
+counter++;
+number.innerHTML=counter+"%";}
+},30)
+let number3=document.getElementById('numberprogresexpenses');
+let counter3=0;
+setInterval(()=>{
+    if(counter3==43){
+    clearInterval();
+    }
+    else{
+counter3++;
+number3.innerHTML=counter3+"%";}
+},30)
+let number2=document.getElementById('numberprogressales');
+let counter2=0;
+setInterval(()=>{
+    if(counter2==19){
+    clearInterval();
+    }
+    else{
+counter2++;
+number2.innerHTML=counter2+"%";}
+},30)
