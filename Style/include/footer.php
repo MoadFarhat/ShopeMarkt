682e2babdad81bfb4c0ad4dce522be6a1e1ba337
MoadFarhat <103571780+MoadFarhat@users.noreply.github.com>
2022-06-22 23:10:26 +0300

    تعديل
    تعديل ملف الأخير
    
    Co-Authored-By: Ahmedalbelazi <81464270+Ahmedalbelazi@users.noreply.github.com>

--- Style/include/footer.php
+++ Style/include/footer.php
@@ -3,24 +3,24 @@
             <h2 class="main-title">الاقسام</h2>
             <div class="container">
                 <div class="box">
-                <a href="search-category.php"><i class="fa-solid fa-car fa-4x"></i></a>
+                <a href="search-category.php?g=السيارات"><i class="fa-solid fa-car fa-4x"></i></a>
                 <!-- <h3>سيارات</h3> -->
                     <div class="info">
-                        <a href="search-category.php">سيارات</a>
+                        <a href="search-category.php?g=السيارات">سيارات</a>
                     </div>
                 </div>
                 <div class="box">
-                <a href="search-category.php"><i class="fa-solid fa-shirt fa-4x"></i></a>
+                <a href="search-category.php?g=ملابس"><i class="fa-solid fa-shirt fa-4x"></i></a>
                 <!-- <h3> ملابس</h3> -->
                     <div class="info">
-                        <a href="search-category.php">ملابس</a>
+                        <a href="search-category.php?g=ملابس">ملابس</a>
                     </div>
                 </div>
                 <div class="box">
-                <a href="search-category.php"><i class="fa-solid fa-tv fa-4x"></i> </a>
+                <a href="search-category.php?g=الاجهزة الكترونية"><i class="fa-solid fa-tv fa-4x"></i> </a>
                 <!-- <h3>الاجهزة الكترونية</h3> -->
                     <div class="info">
-                        <a href="search-category.php">الاجهزة الكترونية</a>
+                        <a href="search-category.php?g=الاجهزة الكترونية">الاجهزة الكترونية</a>
                     </div>
                 </div>
                 <div class="box">
