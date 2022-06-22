0cbc69fafcc77c6a5f569a287d989b771333c8da
MoadFarhat <103571780+MoadFarhat@users.noreply.github.com>
2022-06-22 23:04:40 +0300

    تعديل ملفات
    إضافة صفحة حذف مستخدم
    
    Co-Authored-By: Mohammed1Faraj <106354936+Mohammed1Faraj@users.noreply.github.com>

---
+++ dashboard/DeleteUser.php
@@ -0,0 +1,16 @@
+<?php
+require_once('../php/phpConect/mysql_connact.php');
+if(isset($_GET['d'])){
+$encryption_id=base64_decode($_GET['d']);
+$chiper="AES-128-CTR";//خوارزمية التشفير
+$option=0;
+$encryption_vi='1234567890123456';
+$encryption_key='Moad';
+
+$id=openssl_decrypt($encryption_id,$chiper,$encryption_key,$option,$encryption_vi); 
+$query=$conn_link->query("DELETE FROM user WHERE UserID=$id")or die("error");
+header("location:user.php");
+}
+
+
+?>
