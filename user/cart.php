f8dac652dde57fffcdbf58c0a11686cd3353f2d2
Alibenaisha <alibenaisha3@gmail.com>
2022-06-26 19:48:50 +0200

    تعديل
     سلة

--- user/cart.php
+++ user/cart.php
@@ -1,5 +1,6 @@
-<!DOCTYPE html>
-<html>
+<?php
+include 'style/include/header.php';
+?>
 <head>
   	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
@@ -210,4 +211,6 @@
 
 
 </body>
-</html>
+<?php
+    include 'style/include/footer.php';
+?>
