39a855a92fe3cf538bee6d67da986aa8449c1e1f
MoadFarhat <103571780+MoadFarhat@users.noreply.github.com>
2022-08-10 23:11:28 +0300

    إضافة ملفات
    الأجزاء الأخري
    
    Co-Authored-By: Ahmedalbelazi <81464270+Ahmedalbelazi@users.noreply.github.com>

---
+++ Company/style/include/footer.php
@@ -0,0 +1,57 @@
+<!-- Start Category -->
+<div class="category" id="category">
+            <h2 class="main-title">الاقسام</h2>
+            <div class="container">
+                <div class="box">
+                <a href="search-category.php?g=السيارات"><i class="fa-solid fa-car fa-4x"></i></a>
+                <!-- <h3>سيارات</h3> -->
+                    <div class="info">
+                        <a href="search-category.php?g=السيارات">سيارات</a>
+                    </div>
+                </div>
+                <div class="box">
+                <a href="search-category.php?g=ملابس"><i class="fa-solid fa-shirt fa-4x"></i></a>
+                <!-- <h3> ملابس</h3> -->
+                    <div class="info">
+                        <a href="search-category.php?g=ملابس">ملابس</a>
+                    </div>
+                </div>
+                <div class="box">
+                <a href="search-category.php?g=الاجهزة الكترونية"><i class="fa-solid fa-tv fa-4x"></i> </a>
+                <!-- <h3>الاجهزة الكترونية</h3> -->
+                    <div class="info">
+                        <a href="search-category.php?g=الاجهزة الكترونية">الاجهزة الكترونية</a>
+                    </div>
+                </div>
+                <div class="box">
+                <a href="search-category.php?g=كتب"><i class="fa-solid fa-book fa-4x"></i></a>
+                    <div class="info">
+                        <a href="search-category.php?g=كتب">كتب</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+<!-- End Category -->
+    <!-- Start footer -->
+    <div class="footer" id="footer">
+        <div class="container">
+            <i  class="fa-solid fa-shop logo"></i>
+            <h2>Shope Markt</h2>
+            <div class="icon">
+                <a href="facebook.com/"><i class="fa-brands fa-facebook-f"></i></a> 
+                <a href="twitter.com"><i class="fa-brands fa-twitter"></i></i></a> 
+                <a href="snapchat.com"><i class="fa-brands fa-snapchat"></i></i></a> 
+                <a href="instagram.com"><i class="fa-brands fa-instagram"></i></i></a> 
+                
+            
+            
+            </div>
+            <p>&copy; 2022 <span>Shope Markt</span> جميع الحقوق محفوظة</p>
+            
+        </div>
+    </div>
+    <!-- End footer -->
+    <script src="style/js/bootstrap.min.js"></script>
+    <script src="style/js/jquery.min.js"></script>
+</body>
+</html>
---
+++ Company/style/include/header.php
@@ -0,0 +1,90 @@
+<?php
+require_once("../php/phpConect/chickLogin.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shope Markt</title>
+    <!-- Main Shope Markt Css File -->
+    <link rel="stylesheet" href="Style/css/user.css">
+    <!-- Main bootstrap Css File -->
+    <link rel="stylesheet" href="Style/css/bootstrap.min.css">
+    <!-- Font Awesome Library -->
+    <link rel="stylesheet" href="Style/css/all.min.css">
+    <!-- RenderAll Elements Normally -->
+    <link rel="stylesheet" href="Style/css/normalize.css">
+    <!-- Fonts google -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Playball&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">  
+    <script src="style/js/bootstrap.min.js"></script>
+    <script src="style/js/jquery.min.js"></script>
+    
+    
+</head>
+<body>
+    <!-- Start Header -->
+    <header >
+        <div class="container">
+            <?php
+                if($_SESSION['rank']==="مدير")
+                {
+            ?>
+                    <div class="wrapper">
+                        <a href="../dashboard/index.php"><span>لوحة التحكم  </span></a>
+                    </div>
+                    <div class="wrapper">
+                        <a href="logout.php"><span>تسجيل خروج   </span></a>
+                    </div>
+                        
+            <?php
+                    }else{
+            ?>
+                <div class="icons">
+                    <a href="cart.php"><i class="fa-solid fa-cart-shopping "></i></a> 
+                    <a href="index.php"><i class="fa-regular fa-user"></i></i></a> 
+                </div>
+                <div class="wrapper">
+                        <a href="logout.php"><span>تسجيل خروج   </span></a>
+                </div>
+            <?php  
+                    } 
+            ?>
+        <div class="search"  >
+                <form id="searchFormTop" action="search-category.php" method="post" >
+                    <input type="text" class="searchbox" name="q" id="q" placeholder="بحث">
+                    <span class="search-btn-wrap">
+                        <button class="search-btn" type="submit"><i class="fa fa-search"></i></button>
+                    </span>
+                </form>
+            </div>
+                <h1>Shope Markt</h1>
+            <nav>
+                <ul>
+                    <li><a  href="#footer">حول</a></li>
+                    <li><a  href="#category" >الاقسام</a></li>
+                    <li><a class="active" href="main.php">الرئيسة</a></li>
+                </ul>
+            </nav>
+            <a class="logo" href="main.php">
+                <i class="fa-solid fa-shop"></i>
+                <!-- <img src="image/logo.png" alt="logo"> -->
+            </a>
+        </div>
+    </header>
+    <!-- End Header -->
+    <!-- Start Landing -->
+    <div class="landing" id="heade">
+        <div class="overlay"></div>
+            </div>   
+    </div>
+<!-- End Landing -->
